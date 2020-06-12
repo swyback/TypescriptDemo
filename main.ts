63b6ac76330518ce85fd41dfb36532bf63f03c42
Richie Birkett <Richie.Birkett@airsweb.com>
2020-06-12 13:57:55 +0100

    variables and types

--- main.ts
+++ main.ts
@@ -3,3 +3,18 @@ let message = 'Todays date is ';
 let date: Date = new Date();
 console.log(message + date);
 //console.log(date);
+
+let x = 10;
+const y = 20;
+
+let sum;
+const ttile = 'Typescript tutorial';
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'richie';
+
+let sentence: string = `My name is ${name}
+I am a developer trying to learn typescript`;
+
+console.log(sentence);
